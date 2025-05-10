369ffbd43780d4d1fb4da057f7406882dc413471
zuZaKuAi <125731322+zuZaKuAi@users.noreply.github.com>
2025-05-10 12:42:18 +0900

    Update switch_to_led.ino

--- test/switch_to_led/switch_to_led.ino
+++ test/switch_to_led/switch_to_led.ino
@@ -1,1 +1,21 @@
+const int buttonPin = 5;
+  const int ledPin = 4;
 
+  int buttonState = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin,INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+
+  if(buttonState == HIGH){
+    digitalWrite(ledPin, HIGH);
+  }else {
+    digitalWrite(ledPin, LOW);
+  }
+}
